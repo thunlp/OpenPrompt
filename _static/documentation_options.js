e95780a8511b6712a7a4f772af927b256dd6ea84
ShengdingHu <ShengdingHu@users.noreply.github.com>
2021-10-11 09:51:16 +0000

    update docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.0.1',
+    VERSION: 'v0.0.1-beta',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
