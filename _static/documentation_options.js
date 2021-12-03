e075e28656a5b03fd0e0458333bff7a99fc4920a
ningding97 <ningding97@users.noreply.github.com>
2021-12-03 08:53:27 +0000

    update docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.0.1-beta',
+    VERSION: 'v0.1.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
