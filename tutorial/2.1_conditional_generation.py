03f199100784f9d1e327c26cee80cbe6aca445e5
shengdinghu <shengdinghu@gmail.com>
2022-03-26 16:25:20 +0800

    fix bugs

--- tutorial/2.1_conditional_generation.py
+++ tutorial/2.1_conditional_generation.py
@@ -1,66 +1,55 @@
+
+# # Conditional Generation with Prefix Tuning.
+# In this tutorial, we do conditional generation with prefix tuning template.
+
+# we use WebNLG as an example, as well. Note that the evaluation of generation result should be done
+# by using the scripts provided by https://github.com/Yale-LILY/dart/tree/master/evaluation,
+# Which we do not include in it.
+
 import argparse
 import torch
+
 parser = argparse.ArgumentParser("")
-parser.add_argument("--lr", type=float, default=1e-2)
+parser.add_argument("--lr", type=float, default=5e-5)
 parser.add_argument("--plm_eval_mode", action="store_true")
 parser.add_argument("--model", type=str, default='t5')  # tested model are gpt2/t5
-parser.add_argument("--model_name_or_path", default='../../plm_cache/t5-large')
+parser.add_argument("--model_name_or_path", default='t5-base')
 args = parser.parse_args()
-
+print(args)
 
 from openprompt.data_utils.conditional_generation_dataset import WebNLGProcessor
-
-
 dataset = {}
 dataset['train'] = WebNLGProcessor().get_train_examples("./datasets/CondGen/webnlg_2017/")
 dataset['validation'] = WebNLGProcessor().get_dev_examples("./datasets/CondGen/webnlg_2017/")
 dataset['test'] = WebNLGProcessor().get_test_examples("./datasets/CondGen/webnlg_2017/")
 
 
-
-# ## Construct Template
-#
-# A template can be constructed from the yaml config, but it can also be constructed by directly passing arguments.
-# You can load the plm related things provided by openprompt simply by calling:
-
-# %%
+# load a pretrained model, its tokenizer, its config, and its TokenzerWrapper by one function
 from openprompt.plms import load_plm
-
 plm, tokenizer, model_config, WrapperClass = load_plm(args.model, args.model_name_or_path)
 
-
-# # Try more prompt!
-
-# You can use templates other than manual template, for example the mixedtemplate is a good place to start.
-# In MixedTemplate, you can use {"soft"} to denote a tunable template.
-
-
-
-# Or use a mix template
-from openprompt.prompts import SoftTemplate
-from openprompt.prompts import MixedTemplate
-
-# mytemplate = SoftTemplate(model=plm, tokenizer=tokenizer, text='{"placeholder":"text_a"} {"special": "<eos>"} {"mask"}',num_tokens=100)
-
-# mytemplate = SoftTemplate(model=plm, tokenizer=tokenizer, text='{"placeholder":"text_a"} {"soft"} {"soft"} {"soft"} {"placeholder":"text_b"} {"soft"} {"mask"}.')
-
-mytemplate = MixedTemplate(model=plm, tokenizer=tokenizer, text='{"placeholder":"text_a"} {"soft": "Question:"} {"placeholder":"text_b"}? Is it correct? {"soft"} {"mask"}.')
-
+# Instantiating the PrefixTuning Template !
+from openprompt.prompts.prefix_tuning_template import PrefixTuningTemplate
+# we can use a plain text as the default setting
+# i.e.
+# mytemplate = PrefixTuningTemplate(model=plm, tokenizer=tokenizer)
+# is equal to
+# mytemplate = PrefixTuningTemplate(model=plm, tokenizer=tokenizer, text='{"placeholder":"text_a"} {"mask"}')
+mytemplate = PrefixTuningTemplate(model=plm,  tokenizer=tokenizer, text=' {"placeholder":"text_a"} {"special": "<eos>"} {"mask"} ', using_decoder_past_key_values=False)
 
 # To better understand how does the template wrap the example, we visualize one instance.
-
-wrapped_example = mytemplate.wrap_one_example(dataset['train'][0])  # If you use template 3, don't worry the {"soft": "Question:"} is replace by an empty template, it is used to initialize the mixed template and then removed.
+# You may observe that the example doesn't end with <|endoftext|> token. Don't worry, adding specific end-of-text token
+# is a language-model-specific token. we will add it for you in the TokenizerWrapper once you pass `predict_eos_token=True`
+wrapped_example = mytemplate.wrap_one_example(dataset['train'][0])
 print(wrapped_example)
 
 
-# We provide a `PromptDataLoader` class to help you do all the above matters and wrap them into an `torch.DataLoader` style iterator.
-
-
+# Your can loop over the dataset by yourself by subsequently call mytemplate.wrap_one_example  and WrapperClass().tokenizer()
+# but we have provide a PromptDataLoader for you.
 from openprompt import PromptDataLoader
-
 train_dataloader = PromptDataLoader(dataset=dataset["train"], template=mytemplate, tokenizer=tokenizer,
     tokenizer_wrapper_class=WrapperClass, max_seq_length=256, decoder_max_length=256,
-    batch_size=5,shuffle=True, teacher_forcing=True, predict_eos_token=True,
+    batch_size=5,shuffle=True, teacher_forcing=True, predict_eos_token=True, # be sure to pass predict_eos_token=True if your tempalte doesn't contain one, or you model may fail to stop generation.
     truncate_method="head")
 
 validation_dataloader = PromptDataLoader(dataset=dataset["validation"], template=mytemplate, tokenizer=tokenizer,
@@ -73,42 +62,45 @@ test_dataloader = PromptDataLoader(dataset=dataset["test"], template=mytemplate,
     batch_size=5,shuffle=False, teacher_forcing=False, predict_eos_token=True,
     truncate_method="head")
 
-
-# ## Now is time to build your prompt model!
-# In this section we introduce using prompt to do classification, for other kinds of format, please see
-# `generation_tutorial.ipynb`, `probing_tutorial.ipynb`.
-
-print(next(iter(train_dataloader)))
-# exit()
-
+# load the pipeline model PromptForGeneration.
 from openprompt import PromptForGeneration
-
 use_cuda = True
 prompt_model = PromptForGeneration(plm=plm,template=mytemplate, freeze_plm=True,tokenizer=tokenizer, plm_eval_mode=args.plm_eval_mode)
 if use_cuda:
     prompt_model=  prompt_model.cuda()
 
+
 from transformers import AdamW
+# Follow PrefixTuning（https://github.com/XiangLi1999/PrefixTuning), we also fix the language model
+# only include the template's parameters in training.
 
-
-# Using different optimizer for prompt parameters and model parameters
+no_decay = ["bias", "LayerNorm.weight"]
 optimizer_grouped_parameters = [
-    {'params': [p for n,p in prompt_model.template.named_parameters() if "raw_embedding" not in n]}
+{
+    "params": [p for n, p in mytemplate.named_parameters() if (not any(nd in n for nd in no_decay)) and p.requires_grad],
+    "weight_decay": 0.0,
+},
+{
+    "params": [p for n, p in mytemplate.named_parameters() if any(nd in n for nd in no_decay) and p.requires_grad],
+    "weight_decay": 0.0,
+},
 ]
 
-# optimizer1 = AdamW(optimizer_grouped_parameters1, lr=1e-4)
-optimizer = AdamW(optimizer_grouped_parameters, lr=args.lr)
+
+optimizer = AdamW(optimizer_grouped_parameters, lr=args.lr, eps=1e-8)
 
 from transformers.optimization import get_linear_schedule_with_warmup
 
 tot_step  = len(train_dataloader)*5
 scheduler = get_linear_schedule_with_warmup(optimizer, 0, tot_step)
+
 # We provide generation a generation metric, you can also define your own. Note that it's not directly comparable to WebNLG's scripts evaluation.
 from openprompt.utils.metrics import generation_metric
 # Define evaluate function
 def evaluate(prompt_model, dataloader):
     generated_sentence = []
     groundtruth_sentence = []
+    prompt_model.eval()
 
     for step, inputs in enumerate(dataloader):
         if use_cuda:
@@ -118,6 +110,9 @@ def evaluate(prompt_model, dataloader):
         groundtruth_sentence.extend(inputs['tgt_text'])
     score = generation_metric(generated_sentence, groundtruth_sentence, "sentence_bleu")
     print("test_score", score, flush=True)
+    return generated_sentence
+
+
 
 
 generation_arguments = {
@@ -130,15 +125,15 @@ generation_arguments = {
     "top_p": 0.9,
     "repetition_penalty": 1.0,
     "num_beams": 5,
-    "bad_words_ids": None
+    "bad_words_ids": [[628], [198]]
 }
 
+# training and generation.
 global_step = 0
 tot_loss = 0
 log_loss = 0
-# training and generation.
-tot_loss = 0
 for epoch in range(5):
+    prompt_model.train()
     for step, inputs in enumerate(train_dataloader):
         global_step +=1
         if use_cuda:
@@ -153,7 +148,10 @@ for epoch in range(5):
         if global_step %500 ==0:
             print("Epoch {}, global_step {} average loss: {} lr: {}".format(epoch, global_step, (tot_loss-log_loss)/500, scheduler.get_last_lr()[0]), flush=True)
             log_loss = tot_loss
-evaluate(prompt_model, test_dataloader)
 
+generated_sentence = evaluate(prompt_model, test_dataloader)
 
-# %%
+with open(f"../../Generated_sentence_webnlg_gpt2_{args.plm_eval_mode}.txt",'w') as f:
+    for i in generated_sentence:
+        f.write(i+"\n")
+
