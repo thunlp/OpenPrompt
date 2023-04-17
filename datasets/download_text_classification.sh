b86bece102928fdf3a0be71364059cff200d70ba
Jiahang Li <jiahang.li@itadmins-MacBook-Pro-7.local>
2023-04-16 20:26:53 -0700

    add custom dataset dwmw17

--- datasets/download_text_classification.sh
+++ datasets/download_text_classification.sh
@@ -3,41 +3,46 @@ DIR="./TextClassification"
 mkdir $DIR
 cd $DIR
 
-rm -rf mnli
-wget --content-disposition https://cloud.tsinghua.edu.cn/f/33182c22cb594e88b49b/?dl=1
-tar -zxvf mnli.tar.gz
-rm -rf mnli.tar.gz
+# rm -rf mnli
+# wget --content-disposition https://cloud.tsinghua.edu.cn/f/33182c22cb594e88b49b/?dl=1
+# tar -zxvf mnli.tar.gz
+# rm -rf mnli.tar.gz
 
-rm -rf agnews
-wget --content-disposition https://cloud.tsinghua.edu.cn/f/0fb6af2a1e6647b79098/?dl=1
-tar -zxvf agnews.tar.gz
-rm -rf agnews.tar.gz
+# rm -rf agnews
+# wget --content-disposition https://cloud.tsinghua.edu.cn/f/0fb6af2a1e6647b79098/?dl=1
+# tar -zxvf agnews.tar.gz
+# rm -rf agnews.tar.gz
 
-rm -rf dbpedia
-wget --content-disposition https://cloud.tsinghua.edu.cn/f/362d3cdaa63b4692bafb/?dl=1
-tar -zxvf dbpedia.tar.gz
-rm -rf dbpedia.tar.gz
+# rm -rf dbpedia
+# wget --content-disposition https://cloud.tsinghua.edu.cn/f/362d3cdaa63b4692bafb/?dl=1
+# tar -zxvf dbpedia.tar.gz
+# rm -rf dbpedia.tar.gz
 
-rm -rf imdb
-wget --content-disposition https://cloud.tsinghua.edu.cn/f/37bd6cb978d342db87ed/?dl=1
-tar -zxvf imdb.tar.gz
-rm -rf imdb.tar.gz
+# rm -rf imdb
+# wget --content-disposition https://cloud.tsinghua.edu.cn/f/37bd6cb978d342db87ed/?dl=1
+# tar -zxvf imdb.tar.gz
+# rm -rf imdb.tar.gz
 
-rm -rf SST-2
-wget --content-disposition https://cloud.tsinghua.edu.cn/f/bccfdb243eca404f8bf3/?dl=1
-tar -zxvf SST-2.tar.gz
-rm -rf SST-2.tar.gz
+# rm -rf SST-2
+# wget --content-disposition https://cloud.tsinghua.edu.cn/f/bccfdb243eca404f8bf3/?dl=1
+# tar -zxvf SST-2.tar.gz
+# rm -rf SST-2.tar.gz
 
-rm -rf amazon
-wget --content-disposition https://cloud.tsinghua.edu.cn/f/e00a4c44aaf844cdb6c9/?dl=1
-tar -zxvf amazon.tar.gz
-mv datasets/amazon/ amazon
-rm  -rf ./datasets
-rm -rf amazon.tar.gz
+# rm -rf amazon
+# wget --content-disposition https://cloud.tsinghua.edu.cn/f/e00a4c44aaf844cdb6c9/?dl=1
+# tar -zxvf amazon.tar.gz
+# mv datasets/amazon/ amazon
+# rm  -rf ./datasets
+# rm -rf amazon.tar.gz
 
-rm -rf yahoo_answers_topics
-wget --content-disposition https://cloud.tsinghua.edu.cn/f/79257038afaa4730a03f/?dl=1
-tar -zxvf yahoo_answers_topics.tar.gz
-rm -rf yahoo_answers_topics.tar.gz
+# rm -rf yahoo_answers_topics
+# wget --content-disposition https://cloud.tsinghua.edu.cn/f/79257038afaa4730a03f/?dl=1
+# tar -zxvf yahoo_answers_topics.tar.gz
+# rm -rf yahoo_answers_topics.tar.gz
+
+rm -rf dwmw17
+wget --content-disposition https://raw.githubusercontent.com/t-davidson/hate-speech-and-offensive-language/master/data/labeled_data.csv
+mkdir -p dwmw17
+mv labeled_data.csv dwmw17
 
 cd ..
